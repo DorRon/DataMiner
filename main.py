7b9729e06b5ff7634affd280e5b55315a0239774
Dor <dorxrondel@gmail.com>
2014-11-08 18:06:13 -0500

    added twitter info

--- main.py
+++ main.py
@@ -25,8 +25,19 @@ for i in separated:
 
 tw_user_name = raw_input("Enter your twitter username here: ")
 twitter_url = requests.get("http://twitter.com/" + tw_user_name)
-twitter_html = BeautifulSoup(twitter_url.content)
-print twitter_html
+twitter_html = BeautifulSoup(twitter_url.content) 
+print twitter_html.prettify() #makes html cleaner and more readable in the terminal
+#dwindle down html to parse, since information we're looking for is inside specific div tags
+twitter_bio_div = twitter_html.find_all("p", {"class": "ProfileHeaderCard-bio u-dir"}) 
+twitter_place_div = twitter_html.find_all("div", {"class": "ProfileHeaderCard-location"})
+twitter_url_div = twitter_html.find_all("div", {"class": "ProfileHeaderCard-url "})
+twitter_join_div = twitter_html.find_all("div", {"class": "ProfileHeaderCard-joinDate"})
+print twitter_bio_div, twitter_place_div, twitter_url_div, twitter_join_div
+
+
+	
+
+
 
 
 
