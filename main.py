42e6348ec86231d2566d4dbb96f2ee515360c69a
Dor <dorxrondel@gmail.com>
2014-11-08 17:19:31 -0500

    changed variable names, started twitter

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import requests
 from urllib2 import urlopen
+from bs4 import BeautifulSoup
 
-user_name = raw_input("Enter your facebook username here: ")
-url = "http://graph.facebook.com/" + user_name #to obtain specific username information
-info = urlopen(url).read()
+fb_user_name = raw_input("Enter your facebook username here: ")
+fb_url = "http://graph.facebook.com/" + fb_user_name #to obtain specific username information
+info = urlopen(fb_url).read()
 
 #since we already know the JSON structure
 #we can predict the fields and make the 
@@ -15,6 +17,17 @@ info = urlopen(url).read()
 separated = info.split(",") 
 for i in separated:
 	print i
+
+
+###############################
+######### TWITTER #############
+###############################
+
+tw_user_name = raw_input("Enter your twitter username here: ")
+twitter_url = requests.get("http://twitter.com/" + tw_user_name)
+twitter_html = BeautifulSoup(twitter_url.content)
+print twitter_html
+
 
 
 	
