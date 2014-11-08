de174f7399fdce8286923bd6ddf38c187fb58db0
Dor <dorxrondel@gmail.com>
2014-11-08 15:48:08 -0500

    parsed information for Facebook. organized it into a list for easier handling

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ import json
 user_name = raw_input("Enter your facebook username here: ")
 url = "http://graph.facebook.com/" + user_name #to obtain specific username information
 info = urlopen(url).read()
-print info
 
+#since we already know the JSON structure
+#we can predict the fields and make the 
+#information we obtained more presentable
+#and easier to handle in the future.
+
+#separates the json fields provided
+separated = info.split(",") 
+for i in separated:
+	print i
+
+
+	
